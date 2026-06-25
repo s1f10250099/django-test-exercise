137ff160a778b4f5a6e3a16052c961c3bc2ecd2d
1f10250099 <s1f102500999@iniad.org>
2026-06-25 13:01:09 +0900

    詳細ページを追加

--- todo/views.py
+++ todo/views.py
@@ -21,3 +21,14 @@ def index(request):
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
+
+def detail(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    
+    context = {
+        'task': task
+    }
+    return render(request, 'todo/detail.html', context)
